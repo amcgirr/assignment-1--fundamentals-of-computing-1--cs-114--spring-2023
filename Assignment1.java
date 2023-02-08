70499ee7fc261961430726bd9542532a1e860d2d
amcgirr <mcgirr@hartford.edu>
2023-02-08 13:50:53 -0500

    wrote assignment1 program. created initials out of initials. took 5-character string input and took only the 1st 2nd and 3rd index as variables. took input as Fahrenheit and converted to Celsius. generated a random integer between 32 and 16384. concatenated last steps and printed onto screen.

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,45 @@
 // Import libraries here
+import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment1 {
     public static void main(String[] args) {
-        // Aaron McGirr
+      //creating intiials out of initials
+      System.out.println("       AAAA          MMMMM        MMMMM");
+      System.out.println("      AAAAAA         MMMMMM      MMMMMM");
+      System.out.println("     AAA  AAA        MMM MMM    MMM MMM");
+      System.out.println("    AAA    AAA       MMM  MMM  MMM  MMM");
+      System.out.println("   AAAAAAAAAAAA      MMM   MMMMMM   MMM");
+      System.out.println("  AAA        AAA     MMM            MMM");
+      System.out.println(" AAA          AAA    MMM            MMM");
+      System.out.println("AAA            AAA   MMM            MMM");
+      System.out.println("---------------------------------------");
+
+      //taking 5 character input and reversing it without first and last character
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter a 5-character string");
+
+      String UserInput = input.nextLine();
+      char result1 = UserInput.charAt(1);//input at index 1
+      char result2 = UserInput.charAt(2);//input at index 2
+      char result3 = UserInput.charAt(3);//input at index 3
+
+      //Farenheit to celsius converter
+      Scanner input1 = new Scanner(System.in);
+      System.out.println("Enter a number in Fahrenheit");
+
+      Float Fahrenheit = input1.nextFloat();
+      Float Celsius = (Fahrenheit - 32) * (5 / 9);
+
+      //Random number generator
+      Random generator = new Random();
+      Integer num = generator.nextInt(16384) + 32;
+      System.out.println("Random number generated. Continuing...");
+
+      //Print combination of last steps
+      System.out.println("Your new string is " + Celsius + result3 + result2 + result1 + num);
+
+      input.close();
+      input1.close();
     }
 }
